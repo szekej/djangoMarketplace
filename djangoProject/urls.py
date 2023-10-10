63b2026f178c26c4e1336a89de68a40bcb6de431
Szymon <szekej51@gmail.com>
2023-10-10 21:41:28 +0200

    first app - basic stuff

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view=index, name='index'),
+    path('contact/', view=contact, name='contact_name'),
 ]
