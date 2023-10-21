efe3b201d03467f1b614eab04a6093c7dbf5cf42
Szymon <szekej51@gmail.com>
2023-10-21 20:56:53 +0200

    login option available

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
+
+from .forms import LoginForm
 from .views import signup, index, contact
 
 from django.conf import settings
@@ -10,6 +13,7 @@ app_name = 'core'
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', view=contact, name='contact_name'),
-    path('signup/', signup, name='signup')
+    path('signup/', signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login')
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
