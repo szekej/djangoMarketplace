10bc57a7cc2c69f1ababcedca4f977d95a0a45a8
Szymon <szekej51@gmail.com>
2023-10-23 20:46:31 +0200

    removing items

--- item/urls.py
+++ item/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import detail, new
+from .views import detail, new, delete
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,5 +10,6 @@ app_name = 'item'
 urlpatterns = [
     path('<int:pk>', detail, name='detail'),
     path('new_item/', new, name='new'),
+    path('<int:pk>/delete', delete, name='delete'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
