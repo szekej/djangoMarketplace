ee16ea2c0306bc261aaf01c2d8c978da75fb1616
Szymon <szekej51@gmail.com>
2023-11-06 21:36:00 +0100

    logout functionality

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from .forms import LoginForm
@@ -14,6 +14,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', view=contact, name='contact_name'),
     path('signup/', signup, name='signup'),
-    path('login/', LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login')
+    path('login/', LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
